2ed5608608d2d0f7f24bdcca5467b1e195f2e010
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-11-24 20:26:38 +0530

    sss
    ssss

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 import express from 'express';
 
 const app = express();
-const port =3000;
+const Port =3000;
 
 app.get('/',(req,res) =>
-res.send('our soccer application is running ${port}')
+res.send('our soccer application is running ${Port}')
 
 );
-app.listen(port,()=>
-    console.log('your soccer server is running on port ${port}')
+app.listen(Port,()=>
+    console.log('your soccer server is running on port ${Port}')
 
 );
